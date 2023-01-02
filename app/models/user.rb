452ab0a254eb0461e667f4f028a2f705c88dcd2a
andisiambuku <roselandambuku@gmail.com>
2023-01-02 17:31:10 +0300

    added user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-has_many :user_searches
-
-has_many :product_searches through: :user_searches
-
+    # has_many :product_searches through: :user_searches
+    has_many :user_searches
+    #add user validations
+    validates :username, uniqueness: { case_sensitive: false }
 end
