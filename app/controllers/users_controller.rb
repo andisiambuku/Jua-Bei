03f5b1bb28dcd8e545bbae812514034a40083539
andisiambuku <roselandambuku@gmail.com>
2023-01-02 18:52:48 +0300

    feat: created user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+    def create
+        user = User.create(user_params)
+        if user.valid?
+                render json: { user: UserSerializer.new(user) }, status: :created
+        else
+                render json: { error: 'Failed to create user'}, status: :unprocessable_entity
+        end
+    end
+    private
+    def user_params
+        params.require(:user).permit(:username,:email,:password)
+    end
 end
