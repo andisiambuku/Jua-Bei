95240046a22674f739e527e592f01925e86a2c42
andisiambuku <roselandambuku@gmail.com>
2023-01-05 15:47:16 +0300

    feat: user login, profile and signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #signup endpoint
-  post'signup', to: 'users#create'
+  post'/signup', to: 'users#create'
+  post'/login', to: 'auth#create'
+  get '/profile', to: 'users#profile'
 
   # Defines the root path route ("/")
   # root "articles#index"
