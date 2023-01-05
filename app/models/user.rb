e963ffc35041910beb9b41e178333b328442234c
andisiambuku <roselandambuku@gmail.com>
2023-01-05 15:48:29 +0300

    added secure password feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-    # has_many :product_searches through: :user_searches
+
+    has_many :product_searches, through: :user_searches
     has_many :user_searches
+
+    has_secure_password
     #add user validations
     validates :username, uniqueness: { case_sensitive: false }
 end
