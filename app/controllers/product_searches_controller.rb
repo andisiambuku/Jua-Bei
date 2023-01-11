ea1ee4fcd6c602a0ebd1705ebdf0453c8147098d
andisiambuku <roselandambuku@gmail.com>
2023-01-11 11:23:51 +0300

    feat: user search history method

--- app/controllers/product_searches_controller.rb
+++ app/controllers/product_searches_controller.rb
@@ -1,2 +1,14 @@
 class ProductSearchesController < ApplicationController
+    before_action :authorize, only: [:history,:index]
+
+   
+
+    #method to get the user search history
+    def history
+        render json: current_user.product_searches.uniq(&:search_term), status: :ok    
+    end
+
+
+
+
 end
