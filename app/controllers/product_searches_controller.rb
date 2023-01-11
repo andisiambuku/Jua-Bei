da44d743499b37d408dc0d4470c2dc674bc453b7
andisiambuku <roselandambuku@gmail.com>
2023-01-11 12:09:07 +0300

    feat: user search create method

--- app/controllers/product_searches_controller.rb
+++ app/controllers/product_searches_controller.rb
@@ -1,7 +1,25 @@
 class ProductSearchesController < ApplicationController
-    before_action :authorize, only: [:history,:index]
+    before_action :authorize, only: [:history]
 
-   
+    # method to create a search and get a result 
+    def create
+        title = params[:search_term]
+        search = ProductSearch.find_by(search_term: title)
+
+        if search && search.products.length.zero?
+            search.update(count: 1)
+            search.crawl
+        elsif search
+            search.update(count: search.count + 1 )
+        end
+
+        search ||= ProductSearch.create(search_term: title)
+        UserSearch.create(user_id: current_user.id, search_id: search.id) if logged_in?
+        render json: search.products, status: :ok
+
+
+
+    end
 
     #method to get the user search history
     def history
