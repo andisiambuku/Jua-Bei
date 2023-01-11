d9c16de9cda2e16cc93c70d58c83d6b1fb080b05
andisiambuku <roselandambuku@gmail.com>
2023-01-11 11:14:20 +0300

    feat: product search routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  #health check route for render deployment
+  # get '/health_check', to: proc { [200, {}, ['success']] }
+
   #signup endpoint
   post'/signup', to: 'users#create'
   post'/login', to: 'auth#create'
   get '/profile', to: 'users#profile'
+
+  #product search endpoint
+  post '/search', to: 'searches#create'
+  
+  #product search list endpoint
+  get '/searches', to: 'searches#index'
+
+  #user search history endpoint
+  get'/history', to: 'searches#history'
 
   # Defines the root path route ("/")
   # root "articles#index"
