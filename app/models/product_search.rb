69cc63da2ae7670904e6180c9184dcccdb7a3ef4
andisiambuku <roselandambuku@gmail.com>
2023-01-12 23:05:52 +0300

    feat: creating scraper module

--- app/models/product_search.rb
+++ app/models/product_search.rb
@@ -3,4 +3,11 @@ class ProductSearch < ApplicationRecord
     has_many :user_searches
 
     validates :search_term, presence: true
+
+    #crawler module
+    def scraper
+        s = Scraper.new(search_term,id)
+        s.ebay
+        s.jumia
+    end
 end
