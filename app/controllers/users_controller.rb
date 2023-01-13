8be3e78935d2045e3f7dc0ab72295b4b4c54a3c2
andisiambuku <roselandambuku@gmail.com>
2023-01-13 12:04:01 +0300

    fix: user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
         end
     end
     def profile
-        render json: { user: UserSerializer.new(current_user)}, status: :accepted
+        render json: @user, status: :accepted
     end
 
     private
