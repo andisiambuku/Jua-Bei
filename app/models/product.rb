59a7029ac7e67efdab79f894b498173b1b575a25
andisiambuku <roselandambuku@gmail.com>
2023-01-16 18:00:35 +0300

    fix: method for query

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
     belongs_to :product_search
+
+    def self.search(search_term)
+        Product.joins(:product_search).where("product_searches.search_term = ?", search_term)
+    end
 end
