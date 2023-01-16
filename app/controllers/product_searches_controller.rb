680b31eee8df43072658463f2dc8aaa78576a1d7
andisiambuku <roselandambuku@gmail.com>
2023-01-16 18:02:05 +0300

    fix: method for search

--- app/controllers/product_searches_controller.rb
+++ app/controllers/product_searches_controller.rb
@@ -3,33 +3,41 @@ class ProductSearchesController < ApplicationController
     before_action :authorized, only: [:history]
 
     # method to create a search and get a result 
+    # def create
+    #     title = params[:search_term]
+    #     @search = ProductSearch.find_by(search_term: title)
+
+        
+    #     if @search && @search.products.length.zero?
+    #         @search.update(count: 1)
+    #         # @search.scraper
+    #     elsif search
+    #         @search.update(count: search.count + 1 )
+    #     end
+
+    #     search ||= ProductSearch.create(search_term: title)
+    #      UserSearch.create(user_id: current_user.id, product_search_id: search.id) if logged_in?
+    #     render json: search.products, status: :ok
+        
+        
+    # end
     def create
-        title = params[:search_term]
-        @search = ProductSearch.find_by(search_term: title)
-
-        # if search && search.products.length.zero?
-        if @search && @search.products.length.zero?
-            search.update(count: 1)
-            search.scraper
-        elsif search
-            search.update(count: search.count + 1 )
-        end
-
-        search ||= ProductSearch.create(search_term: title)
-         UserSearch.create(user_id: current_user.id, search_id: search.id) if logged_in?
-        render json: search.products, status: :ok
-        
-        
+        @searched_products = Product.search(params[:search_term])
+        render json: @searched_products
     end
 
     #method to get the user search history
     def history
-        @search = ProductSearch.where(user_id: current_user.id)
-        render json: @search.uniq(&:search_term), status: :ok
+        @user_history = ProductSearch.search_history(current_user.id)
+        render json: @user_history
+      end
+    # def history
+    #     @search = ProductSearch.where(user_id: current_user.id)
+    #     render json: @search.uniq(&:search_term), status: :ok
 
-        # render json: search.product_searches.uniq(&:search_term), status: :ok   
+    #     # render json: search.product_searches.uniq(&:search_term), status: :ok   
         
-    end
+    # end
 
 
 
