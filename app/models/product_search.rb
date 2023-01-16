44f313aa8a2bc18eaf9ef1ff5d3b461ff6bf2a22
andisiambuku <roselandambuku@gmail.com>
2023-01-16 18:00:13 +0300

    fix: method for query

--- app/models/product_search.rb
+++ app/models/product_search.rb
@@ -1,16 +1,21 @@
 class ProductSearch < ApplicationRecord
     has_many :products
     has_many :user_searches
+    attr_accessor :product_search_id
 
     validates :search_term, presence: true
 
-    after_create :scraper
-
-    #crawler module
-    def scraper
-        s = Scraper.new(search_term,id)
-        s.ebay
-        s.jumia
-        s.amazon
-    end
+    def self.search_history(user_id)
+        ProductSearch.joins(:user_searches).where(user_searches: {user_id: user_id})
+      end
+    
 end
+# after_create :scraper
+
+    # #crawler module
+    # def scraper
+    #     s = Scraper.new(search_term,id)
+    #     s.ebay
+    #     s.jumia
+    #     s.amazon
+    # end
