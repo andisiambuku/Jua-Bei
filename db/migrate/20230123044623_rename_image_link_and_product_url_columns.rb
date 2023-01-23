7c898088238961a62513b18068f2203144be8b5c
andisiambuku <roselandambuku@gmail.com>
2023-01-23 08:03:12 +0300

    fix: column names

--- db/migrate/20230123044623_rename_image_link_and_product_url_columns.rb
+++ db/migrate/20230123044623_rename_image_link_and_product_url_columns.rb
@@ -1,6 +1,6 @@
 class RenameImageLinkAndProductUrlColumns < ActiveRecord::Migration[7.0]
   def change
-    rename_column :products, :image_url, :image_url
-    rename_column :products, :product_link, :product_link
+    rename_column :products, :image_link, :image_url
+    rename_column :products, :product_url, :product_link
   end
 end
