d80906eb8d87a1de80b575fd36aeb64b955159d9
andisiambuku <roselandambuku@gmail.com>
2023-01-23 08:06:56 +0300

    fix: migration column names

--- db/migrate/20230120111412_rename_image_and_product_columns.rb
+++ db/migrate/20230120111412_rename_image_and_product_columns.rb
@@ -1,6 +1,6 @@
 class RenameImageAndProductColumns < ActiveRecord::Migration[7.0]
   def change
-    rename_column :products, :image_url, :product_link
-    rename_column :products, :product_link, :image_url
+    rename_column :products, :product_url, :product_link
+    rename_column :products, :image_link, :image_url
   end
 end
